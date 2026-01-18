67c19974441ad0bbf6e7b21ba4c24474d7396ea9
Daniel Miranda <dmiran12@calpoly.edu>
2026-01-17 22:57:06 -0800

    feat: added jumping ability

--- Player.js
+++ Player.js
@@ -4,6 +4,12 @@ export default class Player {
     walkAnimationTimer = this.WALK_ANIMATION_TIMER
     dinoRunImages = [];
 
+    jumpPressed = false;
+    jumpInProgress = false;
+    falling = false;
+    JUMP_SPEED = 0.6;
+    GRAVITY = 0.4;
+
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio){
         this.ctx = ctx;
         this.canvas = ctx.canvas
@@ -15,6 +21,7 @@ export default class Player {
 
         this.x = 10 * scaleRatio;
         this.y = this.canvas.height - this.height - 1.5 * scaleRatio
+        this.yStandingPosition = this.y;
 
         this.standingStillImage = new Image();
         this.standingStillImage.src = "images/standing_still.png";
@@ -28,10 +35,79 @@ export default class Player {
 
         this.dinoRunImages.push(dinoRunImage1);
         this.dinoRunImages.push(dinoRunImage2);
+
+        //keyboard
+
+        window.removeEventListener('keydown', this.keydown);
+        window.removeEventListener('keyup', this.keyup);
+
+        window.addEventListener('keydown', this.keydown);
+        window.addEventListener('keyup', this.keyup);
+
+        //touch
+        window.removeEventListener('touchstart', this.touchstart);
+        window.removeEventListener('touchend', this.touchend);
+
+        window.addEventListener('touchstart', this.touchstart);
+        window.addEventListener('touchend', this.touchend);
+    }
+
+    touchstart = () => {
+        this.jumpPressed = true;
+    }
+
+    touchend = () => {
+        this.jumpPressed = false;
+    }
+
+    
+
+    keydown = (event)=>{ // learn arrow functions
+        if(event.code === "Space") {
+            this.jumpPressed = true;
+        }
+    }
+
+    keyup = (event) => {
+        if (event.code === "Space") {
+            this.jumpPressed = false;
+        }
     }
 
     update(gameSpeed, frameTimeDelta){
         this.run(gameSpeed, frameTimeDelta);
+        if(this.jumpInProgress){
+            this.image = this.standingStillImage;
+        }
+        this.jump(frameTimeDelta);
+    }
+
+    jump(frameTimeDelta){
+        if(this.jumpPressed) {
+            this.jumpInProgress = true;
+        }
+
+        if(this.jumpInProgress && !this.falling){
+            if(this.y > this.canvas.height - this.minJumpHeight ||
+                (this.y > this.canvas.height - this.maxJumpHeight && this.jumpPressed)
+            ) {
+                this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
+            } else {
+                this.falling = true;
+            }
+        }
+        else{
+            if(this.y < this.yStandingPosition){
+                this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+                if(this.y + this.height > this.canvas.height){
+                    this.y = this.yStandingPosition;
+                }
+            }
+            else{
+                this.falling = false;
+                this.jumpInProgress = false;
+            }
+        }
     }
 
     run(gameSpeed, frameTimeDelta){
